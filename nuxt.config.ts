f920ea971141f1391363785de781deaf0d3c7db2
alexbulanovgoodmail <alexbulanovgoodmail@gmail.com>
2023-12-25 12:10:39 +0300

    Анимация при изменении страниц

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@
 export default defineNuxtConfig({
 	devtools: { enabled: true },
 
+	app: {
+		pageTransition: { name: 'page', mode: 'out-in' }
+	},
+
 	modules: ['@nuxtjs/eslint-module', '@nuxtjs/stylelint-module', '@nuxt/ui'],
 
 	eslint: {
