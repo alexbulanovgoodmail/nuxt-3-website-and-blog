1821b929527423ba2a00c5c02acb5dbbcdf25f37
alexbulanovgoodmail <alexbulanovgoodmail@gmail.com>
2023-12-25 13:50:04 +0300

    Панель администратора. Создание структуры страниц

---
+++ layouts/admin.vue
@@ -0,0 +1,24 @@
+<script setup>
+const isOpen = ref(false)
+</script>
+
+<template>
+	<div class="my-14 lg:my-24">
+		<UContainer>
+			<UButton label="Открыть" @click="isOpen = true" />
+		</UContainer>
+
+		<div class="my-4 lg:my-6">
+			<slot />
+		</div>
+
+		<USlideover v-model="isOpen">
+			<div>
+				<UButton label="Закрыть" @click="isOpen = false" />
+			</div>
+			<div class="p-4 flex-1">
+				<div class="h-full"></div>
+			</div>
+		</USlideover>
+	</div>
+</template>
---
+++ pages/admin/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+definePageMeta({
+	layout: 'admin'
+})
+</script>
+
+<template>
+	<div>
+		<UContainer>
+			<h1>Welcome to admin page</h1>
+		</UContainer>
+	</div>
+</template>
